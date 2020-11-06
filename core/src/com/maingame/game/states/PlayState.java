f7bf542184f86817cc582ea217d3aefb25f91745
Omar Omar <omarhatem5555@gmail.com>
2020-11-06 16:29:26 +0000

    Created PlayState

---
+++ core/src/com/maingame/game/states/PlayState.java
@@ -0,0 +1,12 @@
+package com.maingame.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class PlayState {
+    private Texture bg;
+    private Texture boat;
+
+    public PlayState(GameStateManager gsm) {
+
+    }
+}
