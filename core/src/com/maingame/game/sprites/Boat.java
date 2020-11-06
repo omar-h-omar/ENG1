4f41af10f0d2da94f8f5492d0de7660019d33713
DESKTOP-H52BC81\owner <re744@york.ac.uk>
2020-11-06 20:55:20 +0000

    New Boat Class

--- core/src/com/maingame/game/sprites/Boat.java
+++ core/src/com/maingame/game/sprites/Boat.java
@@ -3,26 +3,65 @@ import com.badlogic.gdx.math.Rectangle;
 
 import java.util.List;
 import java.util.HashMap;
+import java.util.ArrayList;
 
-public class Boat{
+public class Boat {
     //Attributes
     private String colour, img;
     private Rectangle bounds;
     private int velocityX, velocityY, maneuverability, robustness, maxAcceleration, health = 100;
     private boolean isPlayer;
 
-    List<String> Available;
-    HashMap<String, List<Integer>>  BoatMap;
+    List<String> Available = new ArrayList<String>();
+    Available.add("red");
+    Available.add("pink");
+    Available.add("blue");
+    Available.add("yellow");
+    Available.add("orange");
+    Available.add("green");
+    Available.add("purple");
 
-    HashMap<String, String> BoatImg;
+    HashMap<String, Integer[]>  BoatMap = new HashMap<String, Integer[]>();
+
+    HashMap<String, String[]> BoatImg = new HashMap<String, String[]>();
+
+    String[] arr = {"Boat1.1.png", "Boat1.2.png"};
+    BoatImg.put("red", arr);
+    arr = {"Boat2.1.png", "Boat2.2.png"};
+    BoatImg.put("pink", arr);
+    arr = {"Boat2.1.png", "Boat2.2.png"};
+    BoatImg.put("pink", arr);
+    arr = {"Boat3.1.png", "Boat3.2.png"};
+    BoatImg.put("blue", arr);
+    arr = {"Boat4.1.png", "Boat4.2.png"};
+    BoatImg.put("yellow", arr);
+    arr = {"Boat5.1.png", "Boat5.2.png"};
+    BoatImg.put("orange", arr);
+    arr = {"Boat6.1.png", "Boat6.2.png"};
+    BoatImg.put("green", arr);
+    arr = {"Boat7.1.png", "Boat7.2.png"};
+    BoatImg.put("purple", arr);
+
 
 
     public Boat(String col){
         colour = col;
-        maneuverability = BoatMap.get(col).get(0);
-        robustness = BoatMap.get(col).get(1);
-        maxAcceleration =  BoatMap.get(col).get(2);
-        img = BoatImg.get(col);
+        maneuverability = BoatMap.get(col)[0];
+        robustness = BoatMap.get(col)[1];
+        maxAcceleration =  BoatMap.get(col)[2];
+        img = BoatImg.get(col)[0];
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int h){
+        health = h;
+    }
+
+    public boolean collide(object obj){
+        return false;
     }
 
 }
