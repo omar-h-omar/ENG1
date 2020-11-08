6c09c1a0b16aaa93293854cc6337fecf3d67d584
Omar Omar <omarhatem5555@gmail.com>
2020-11-08 15:09:56 +0000

    Fixed the bugs found in Boat class

--- core/src/com/maingame/game/sprites/Boat.java
+++ core/src/com/maingame/game/sprites/Boat.java
@@ -1,56 +1,57 @@
 package com.maingame.game.sprites;
 import com.badlogic.gdx.math.Rectangle;
 
-import java.util.List;
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public class Boat {
     //Attributes
-    private String colour, img;
+    public String colour, img;
     private Rectangle bounds;
     private int velocityX, velocityY, maneuverability, robustness, maxAcceleration, health = 100;
     private boolean isPlayer;
+    private HashMap<String, String[]> BoatImg = new HashMap<String, String[]>();
+    private HashMap<String, Integer[]>  BoatMap = new HashMap<String, Integer[]>();
 
-    List<String> available = new ArrayList<String>();
-    available.add("red");
-    available.add("pink");
-    available.add("blue");
-    available.add("yellow");
-    available.add("orange");
-    available.add("green");
-    available.add("purple");
-
-    HashMap<String, Integer[]>  BoatMap = new HashMap<String, Integer[]>();
-
-    HashMap<String, String[]> BoatImg = new HashMap<String, String[]>();
-
-    String[] arr = {"Boat1.1.png", "Boat1.2.png"};
-    BoatImg.put("red", arr);
-    arr = {"Boat2.1.png", "Boat2.2.png"};
-    BoatImg.put("pink", arr);
-    arr = {"Boat2.1.png", "Boat2.2.png"};
-    BoatImg.put("pink", arr);
-    arr = {"Boat3.1.png", "Boat3.2.png"};
-    BoatImg.put("blue", arr);
-    arr = {"Boat4.1.png", "Boat4.2.png"};
-    BoatImg.put("yellow", arr);
-    arr = {"Boat5.1.png", "Boat5.2.png"};
-    BoatImg.put("orange", arr);
-    arr = {"Boat6.1.png", "Boat6.2.png"};
-    BoatImg.put("green", arr);
-    arr = {"Boat7.1.png", "Boat7.2.png"};
-    BoatImg.put("purple", arr);
-
-
+//    TODO: figure out if we need the available list or if we can add it to the menu or play states
+//    List<String> available = new ArrayList<String>();
+//    available.add("red");
+//    available.add("pink");
+//    available.add("blue");
+//    available.add("yellow");
+//    available.add("orange");
+//    available.add("green");
+//    available.add("purple");
 
     public Boat(String col){
+        this.buildBoatData();
         colour = col;
-        maneuverability = BoatMap.get(col)[0];
-        robustness = BoatMap.get(col)[1];
-        maxAcceleration =  BoatMap.get(col)[2];
+//        maneuverability = BoatMap.get(col)[0];
+//        robustness = BoatMap.get(col)[1];
+//        maxAcceleration =  BoatMap.get(col)[2];
         img = BoatImg.get(col)[0];
-        available.remove(col);
+//        available.remove(col);
+    }
+//  TODO: Add the attributes for each boat like velocity, ..etc in boatMap
+//  Builds the hashmaps for the boat data.
+//  BoatImg contains the images for the boats in an array of strings. The strings are the file names of the images.
+//  BoatMap contains the attributes for each boat in an array of integers.
+    private void buildBoatData() {
+        String[] arr = {"Boat1.1.png", "Boat1.2.png"};
+        BoatImg.put("red", arr);
+        arr = new String[]{"Boat2.1.png", "Boat2.2.png"};
+        BoatImg.put("pink", arr);
+        arr = new String[]{"Boat2.1.png", "Boat2.2.png"};
+        BoatImg.put("pink", arr);
+        arr = new String[]{"Boat3.1.png", "Boat3.2.png"};
+        BoatImg.put("blue", arr);
+        arr = new String[]{"Boat4.1.png", "Boat4.2.png"};
+        BoatImg.put("yellow", arr);
+        arr = new String[]{"Boat5.1.png", "Boat5.2.png"};
+        BoatImg.put("orange", arr);
+        arr = new String[]{"Boat6.1.png", "Boat6.2.png"};
+        BoatImg.put("green", arr);
+        arr = new String[]{"Boat7.1.png", "Boat7.2.png"};
+        BoatImg.put("purple", arr);
     }
 
     public int getHealth(){
@@ -61,8 +62,8 @@ public class Boat {
         health = h;
     }
 
-    public boolean collide(object obj){
-        return false;
-    }
+//    public boolean collide(object obj){
+//        return false;
+//    }
 
 }
