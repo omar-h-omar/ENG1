27e33c4a47f4e0d78fc8fde9f9dffed781de20ae
Omar Omar <omarhatem5555@gmail.com>
2020-11-08 15:40:58 +0000

    menu state

--- core/src/com/maingame/game/states/MenuState.java
+++ core/src/com/maingame/game/states/MenuState.java
@@ -3,15 +3,23 @@ package com.maingame.game.states;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.maingame.game.MainGame;
+import com.maingame.game.sprites.Boat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuState extends State {
 	private Texture background;
-	private Texture playBtn;
+	private List<Boat> boats = new ArrayList<Boat>();;
 
 	public MenuState(GameStateManager gsm) {
 		super(gsm);
 		background = new Texture("background.PNG");
-		playBtn = new Texture("play.PNG");
+		this.buildBoats();
+	}
+
+	private void buildBoats() {
+		boats.add(new Boat("red"));
 	}
 
 	@Override
@@ -28,7 +36,9 @@ public class MenuState extends State {
 	public void render(SpriteBatch sb) {
 		sb.begin();
 		sb.draw(background, 0, 0, MainGame.WIDTH , MainGame.HEIGHT);
-		sb.draw(playBtn, (MainGame.WIDTH / 2) - (playBtn.getWidth() / 2), MainGame.HEIGHT / 2);
+		Texture img = new Texture(boats.get(0).img);
+		sb.draw(img,20,20);
+//		sb.draw(playBtn, (MainGame.WIDTH / 2) - (playBtn.getWidth() / 2), MainGame.HEIGHT / 2);
 		sb.end();
 	}
 
