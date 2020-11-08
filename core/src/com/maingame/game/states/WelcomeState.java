5744d39b6fbda90f406927d18c41403945ee1e3f
okan-d <72575596+okan-d@users.noreply.github.com>
2020-11-08 15:48:43 +0000

    State switch

--- core/src/com/maingame/game/states/WelcomeState.java
+++ core/src/com/maingame/game/states/WelcomeState.java
@@ -1,5 +1,6 @@
 package com.maingame.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.maingame.game.MainGame;
@@ -19,12 +20,15 @@ public class WelcomeState extends State {
 
 	@Override
 	public void handleInput() {
-		
+		if (Gdx.input.justTouched()) {
+			gsm.set(new MenuState(gsm));
+			dispose();
+		}
 	}
 	
 	@Override
 	public void update(float dt) {
-		
+		handleInput();
 	}
 	
 	@Override
