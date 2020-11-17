44bcf17022883a480ccd33bc74a657a141271deb
DESKTOP-H52BC81\owner <re744@york.ac.uk>
2020-11-17 02:30:36 +0000

    New Obstacles class

--- core/src/com/maingame/game/sprites/MoveableObstacles.java
+++ core/src/com/maingame/game/sprites/MoveableObstacles.java
@@ -3,11 +3,15 @@ package com.maingame.game.sprites;
 
 public class MoveableObstacles extends Obstacles{
         protected int velocity;
-        protected int[] direction;
-
 
     public MoveableObstacles(String n) {
         super(n);
+    }
+
+    public void move(){
+
+        setPosition(posX + velocity, posY);
+
     }
 }
 
--- core/src/com/maingame/game/sprites/Obstacles.java
+++ core/src/com/maingame/game/sprites/Obstacles.java
@@ -1,21 +1,63 @@
 package com.maingame.game.sprites;
 
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class Obstacles {
-    protected String name, img;
-    protected int impactDamage;
+class Obstacles {
+    protected String name;
+    protected Texture img;
+    protected int impactDamage, posX, posY;
 
-   //HashMap<String, String> ObstacleImg = new HashMap<String, String>();
+    HashMap<String, Texture> ObstacleImg = new HashMap<String, Texture>();
+    HashMap<String, Integer> ObstacleDamage = new HashMap<String, Integer>();
 
 
-    public Obstacles(String n){
+    public Obstacles(String n) {
         name = n;
+        buildObstacleData();
+        img = ObstacleImg.get(n);
+        impactDamage = ObstacleDamage.get(n);
+
     }
 
-    public boolean checkHit(){
+    public boolean checkHit() {
         return false;
     }
+
+    private void buildObstacleData() {
+
+        Texture a = new Texture("rock1.png");
+        ObstacleImg.put("rock1", a);
+
+        a = new Texture("rock2.png");
+        ObstacleImg.put("rock2", a);
+
+        a = new Texture("goose.png");
+        ObstacleImg.put("goose", a);
+
+        a = new Texture("duck1.png");
+        ObstacleImg.put("duck1", a);
+
+        a = new Texture("duck2.png");
+        ObstacleImg.put("duck2", a);
+
+        ObstacleDamage.put("rock1", 10);
+        ObstacleDamage.put("rock2", 10);
+        ObstacleDamage.put("goose", 15);
+        ObstacleDamage.put("duck1", 5);
+        ObstacleDamage.put("duck2", 5);
+
+    }
+
+    public void setPosition(int a, int b){
+        posX = a;
+        posY = b;
+    }
+
+}
 
 
 }
